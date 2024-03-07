2ec566418e86143958752ed67409680cd9ba57ff
YoRu Tech <tech.niko.coding@gmail.com>
2024-03-07 13:17:35 +0500

    Final Version - Completely Responsive for all Devices - Animations Added

--- js/script.js
+++ js/script.js
@@ -25,6 +25,12 @@ window.onscroll = () => {
 				link.classList.remove('active');
 				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
 			});
+			//active sections for animation on scroll
+			sec.classList.add('show-animate');
+		}
+		//if want to use animation that repeats on scroll use this
+		else {
+			sec.classList.remove('show-animate');
 		}
 	});
 
@@ -32,4 +38,12 @@ window.onscroll = () => {
   let header = document.querySelector('header');
 
 	header.classList.toggle('sticky', window.scrollY > 100);
+
+	//remove toggle icon and navbar when click navbar (scroll)
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+
+	//animation footer on scroll 
+	let footer = document.querySelector('footer');
+	footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
